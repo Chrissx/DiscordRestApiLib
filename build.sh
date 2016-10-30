1b9ebc9874dd460d41d12eeb9dcf875379d2eeb0
Krisztián Bodrogi <master.994@gmail.com>
2016-10-30 12:48:02 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  
+repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
 
-koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"  
-if [ ! -z $KOREBUILD_ZIP ]; then  
+koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
+if [ ! -z $KOREBUILD_ZIP ]; then
     koreBuildZip=$KOREBUILD_ZIP
 fi
 
-buildFolder=".build"  
+buildFolder=".build"
 buildFile="$buildFolder/KoreBuild.sh"
 
-if test ! -d $buildFolder; then  
+if test ! -d $buildFolder; then
     echo "Downloading KoreBuild from $koreBuildZip"
-
+    
     tempFolder="/tmp/KoreBuild-$(uuidgen)"    
     mkdir $tempFolder
-
+    
     localZipFile="$tempFolder/korebuild.zip"
-
+    
     retries=6
     until (wget -O $localZipFile $koreBuildZip 2>/dev/null || curl -o $localZipFile --location $koreBuildZip 2>/dev/null)
     do
@@ -29,14 +29,14 @@ if test ! -d $buildFolder; then
         echo "Waiting 10 seconds before retrying. Retries left: $retries"
         sleep 10s
     done
-
+    
     unzip -q -d $tempFolder $localZipFile
-
+  
     mkdir $buildFolder
     cp -r $tempFolder/**/build/** $buildFolder
-
+    
     chmod +x $buildFile
-
+    
     # Cleanup
     if test ! -d $tempFolder; then
         rm -rf $tempFolder  
